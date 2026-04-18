891a84a010be144f607a330fc80c303519ddfe62
LucaRotter <lgrotter@studenti.uninsubria.it>
2026-04-18 10:57:39 +0200

    aggiunta metodi

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,20 @@ use std::fs::File;
 use std::io::Write;
 fn main() {
     let mut lista_task: Vec<Task> = caricamento_da_file();
-
+    loop{
+    //chiedi all'utente cosa vuole fare
     let comando: u32 = chiedi_comando_base();
+    //match del comando scelto
     match comando {
-        0 => aggiungi_task(),
-        1 => visualizza_task(),
-        2 => println!("termine programma, arrivederci! :)"),
+        0 => aggiungi_task(&mut lista_task),
+        1 => visualizza_task(&mut lista_task),
+        2 => {
+            println!("termine programma, arrivederci! :)"); 
+            scrittura_su_file(&lista_task);    
+            break;},
         _ => println!("Comando non valido, ma il filtro nel loop dovrebbe averlo evitato!"),
     }
+}
 }
 
 fn chiedi_comando_base() -> u32 {
@@ -32,8 +38,95 @@ fn chiedi_comando_base() -> u32 {
     }
 }
 
-fn aggiungi_task() {}
-fn visualizza_task() {}
+fn aggiungi_task(lista_task: &mut Vec<Task>) {
+    let lunghezza_lista_iniziale: usize = lista_task.len();
+    println!("inserisci il nome della task da aggiungere:");
+    let mut task = String::new();
+    io::stdin().read_line(&mut task).unwrap();
+    let nuova_task = Task{
+        id: lista_task.len() as u32 + 1,
+        testo: task.trim().to_string(),
+        completato: false,
+    };
+    &mut lista_task.push(nuova_task);
+        if lista_task.len() > lunghezza_lista_iniziale {
+            println!("Task aggiunta con successo!");
+        } else {
+            println!("Errore: Task non aggiunta.");
+        }
+}
+fn visualizza_task(lista_task: &mut Vec<Task>) {
+    if lista_task.is_empty() {
+        println!("Non ci sono task da visualizzare.");
+    } else {
+        println!("Ecco le task da fare:");
+        for task in lista_task.iter() {
+            println!("ID: {}, Testo: {}, Completato: {}", task.id, task.testo, task.completato);
+        }
+    }
+    println!("vuoi modificare lo stato di qualche task? (s/n)");
+    let mut risposta = String::new();
+    io::stdin().read_line(&mut risposta).unwrap();
+    match risposta.trim().to_lowercase().as_str() {
+        "s" => modifica_stato_task(lista_task),
+        "n" => println!("Nessuna modifica alle task."),
+        _ => println!("Risposta non valida, nessuna modifica alle task."),
+    };
+}
+fn modifica_stato_task(lista_task: &mut Vec<Task>) {
+    println!("inserisci l'ID della task da modificare:");
+    let mut id_input_dacercare: u32 = 0;
+
+    loop{
+        let mut id_input = String::new();
+        io::stdin().read_line(&mut id_input).unwrap();
+
+        match id_input.trim().parse::<u32>() {
+            Ok(num) => {
+                id_input_dacercare = num;
+                break;
+            }
+            Err(_) => println!("Errore: Inserisci un numero intero valido per l'ID, per favore."),
+        }
+    }
+
+    for task in lista_task.iter_mut(){
+        let mut flag: bool = false;
+        if task.id == id_input_dacercare {
+            flag = true;
+            let comp: bool = task.completato;
+            if comp == false {
+                println!("la task con ID {} è attualmente incompleta, vuoi completarla? (s/n)", task.id);ù
+                let mut risposta: String = String::new();
+                io::stdin().read_line(&mut risposta).unwrap();
+                match risposta.trim().to_lowercase().as_str() {
+                    "s" => {task.completato = true;
+                        println!("Task con ID {} completata!", task.id);
+                    },
+                    "n" => println!("Nessuna modifica alla task con ID {}.", task.id),
+                    _ => println!("Risposta non valida, nessuna modifica alla task con ID {}.", task.id),
+                };
+                break;
+            }
+            else{
+                println!("la task con ID {} è attualmente completata, vuoi renderla incompleta? (s/n)", task.id);
+                let mut risposta: String = String::new();
+                io::stdin().read_line(&mut risposta).unwrap();
+                match risposta.trim().to_lowercase().as_str() {
+                    "s" => {task.completato = false;
+                        println!("Task con ID {} resa incompleta!", task.id);
+                    },
+                    "n" => println!("Nessuna modifica alla task con ID {}.", task.id),
+                    _ => println!("Risposta non valida, nessuna modifica alla task con ID {}.", task.id),
+                };
+                break;
+            }
+        }
+    
+    if flag == false {
+        println!("Errore, Non esiste nessuna task con ID {}.", id_input_dacercare);
+    }
+}
 fn caricamento_da_file() -> Vec<Task> {
     let nome_file: &str = "tasks.json";
     //lettura normale del file in testo
--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Task {
-    id: u32,
-    testo: String,
-    completato: bool,
+    pub id: u32,
+    pub testo: String,
+    pub completato: bool,
 }
